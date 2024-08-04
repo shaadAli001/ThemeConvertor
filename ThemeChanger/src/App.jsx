93bb46f9e437705f66df4d4713c2781eb61ada89
shaadAli001 <alishaad908@gmail.com>
2024-08-04 15:01:35 +0530

    Commit

--- ThemeChanger/src/App.jsx
+++ ThemeChanger/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 className='p-4 bg-gray-600 text-center'>Theme Changer</h1>
-    </>
-  )
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+
+        <div className="w-full max-w-sm mx-auto"></div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
